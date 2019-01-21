1e99a24d28c16f7ca5c7867106c383c03ce81b1e
Aitawade <shruti.aitawade@atos.net>
2019-01-21 09:57:11 +0530

    Inventry component update inventry implementation

--- src/app/admin/inventry/update-inventry/update-inventry.component.ts
+++ src/app/admin/inventry/update-inventry/update-inventry.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'app/shared/models/Book';
 import { InventryService } from 'app/shared/services/inventry.service';
+import { Observable } from 'rxjs';
+
+import { ActivatedRoute } from '@angular/router';
+import { Type } from 'app/shared/models/Type';
 
 @Component({
   selector: 'app-update-inventry',
@@ -9,16 +13,22 @@ import { InventryService } from 'app/shared/services/inventry.service';
 })
 export class UpdateInventryComponent implements OnInit {
 
-  constructor(private inventryService:InventryService) { }
+  constructor(private inventryService:InventryService, private route: ActivatedRoute) { }
 
   listOfBook: Book[];
+  id: string;
+  listOfType: Type[];
+
+  selectedBook;
 
   ngOnInit() {
     this.inventryService.getAllBooks().subscribe(data=>{
       this.listOfBook=data;
     });
-    console.log(this.listOfBook);
-
-  }
-
+  } 
+  getType(event: any){
+      console.log(this.selectedBook);
+      this.inventryService.getType(this.selectedBook).subscribe(data=> this.listOfType = data);
+    }  
+   
 }
--- src/app/shared/services/inventry.service.ts
+++ src/app/shared/services/inventry.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { AppSettings } from 'app/appSettings';
 import { Observable } from 'rxjs';
 import { Book } from '../models/Book';
+import { Type } from '../models/Type';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,13 @@ export class InventryService {
    }
 
    private listOfBooks=AppSettings.API_ENDPOINT+'/bookName/';
+   private Type=AppSettings.API_ENDPOINT+'/bookName/getbook/';
 
    getAllBooks():Observable<Book[]>{
      return this.httpClient.get<Book[]>(this.listOfBooks);
+   }
+
+   getType(id: number):Observable<Type[]>{
+     return this.httpClient.get<Type[]>(this.Type+id);
    }
 }
