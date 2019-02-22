43a05368ec1f02b2cfefd9e37d00183403dbbca0
Prasad Dukale <prasad.dukale@atos.net>
2019-02-22 11:03:17 +0530

    updated new book request

--- src/app/shared/models/NewBookModel.ts
+++ src/app/shared/models/NewBookModel.ts
@@ -1,16 +1,17 @@
+import { User } from './User';
 
 export class NewBook {
 
     constructor(
-        public id: number,
-        public bookName: string,
-        public language: string,
-        public bookType: string,
-        public price: number,
-        public abbrivation: string,
-        public user: string,
-        public status: string,
-        public remark: string
+        public $id: number,
+        public $bookName: string,
+        public $language: string,
+        public $bookType: string,
+        public $price: number,
+        public $abbrivation: string,
+        public $user: User,
+        public $status: string,
+        public $remark: string
     ) {
     }
 }
