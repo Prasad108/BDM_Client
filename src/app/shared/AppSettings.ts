682a25ecfdf47a3d747f994ac2a5f90ea9c6bfc4
prasad108 <prasad.dukale@atos.net>
2019-04-02 20:45:17 +0530

    added webpack-bundle-analyzer

--- src/app/shared/AppSettings.ts
+++
@@ -1,9 +0,0 @@
-import { HttpHeaders } from '@angular/common/http';
-
-export class AppSettings {
-    public static API_ENDPOINT = 'http://localhost:8080';
-
-    public static HTTP_OPTIONS = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
- }
